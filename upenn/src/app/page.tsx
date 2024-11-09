65fb0190b38b12fe40759c2b785a11b46927e6ec
Bruce Wang <bruce.wang15@outlook.com>
2024-11-09 14:21:28 -0500

    made things into components

--- upenn/src/app/page.tsx
+++ upenn/src/app/page.tsx
@@ -1,17 +1,12 @@
 "use client"
 
-import { Boxes } from '@/components/ui/background-boxes';
 import './page.css'
-import {GlareCard} from "@/components/ui/glare-card";
 import Header from "../components/docs/Header";
 import FrontPage from '../components/docs/FrontPage';
 import TeamMembers from '../components/docs/TeamMembers';
 import AboutUs from '../components/docs/AboutUs';
 
-import { motion } from "framer-motion";
-import {EvervaultCard, Icon } from "@/components/ui/evervault-card";
-import {LampContainer} from "@/components/ui/lamp";
-import {cn} from "@/lib/utils";  // Adjust the import path if necessary
+
 
 export default function Home() {
 
